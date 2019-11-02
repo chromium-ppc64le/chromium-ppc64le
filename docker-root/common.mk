a7a769b225f0ecefd8b25bf1b19a1acd51f40c8f
Colin Samples <colin.samples+git@gmail.com>
2019-11-02 18:41:16 -0400

    update to 78.0.3904.87

--- docker-root/common.mk
+++ docker-root/common.mk
@@ -6,7 +6,7 @@
 # llvm_rev is from $(chrome-dir)/tools/clang/scripts/update.py
 llvm_rev := 8455294f2ac13d587b13d728038a9bffa7185f2b
 cpf_rev := d6b7ab92474eb27d773d3e4578a1e8bd71586075
-chrome_ver := 78.0.3904.70
+chrome_ver := 78.0.3904.87
 # the rpm release number is hardcoded in
 # $(chrome-dir)/chrome/installer/linux/common/installer.include, but it should
 # be bumped when releasing a new rpm with the same version number
